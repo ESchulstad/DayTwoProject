b8ef176a3be6569d13fedec3db1ab096c5c7653e
ESchulstad <eschulstad@gmail.com>
2016-11-07 21:04:39 -0500

    initial commit

---
+++ DayTwoProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTwoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 3;
+
+            if(x<=y)
+            {
+                Console.WriteLine(x+" is less than or equal to "+y);
+            }
+            else
+            {
+                Console.WriteLine(x + " is NOT less than or equal to " + y);
+            }
+
+
+            //continue with normal program execution
+
+        }
+    }
+}
